fcc29777bf44cf518613c5292f1911b306f77ecc
Edmund Dipple <elmundio1987@gmail.com>
2018-10-08 20:06:28 +0100

    initial commit

---
+++ add_torrent.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+deluge_password=$1
+magnet_url=$2
+move_completed_path=$3
+
+curl  --silent --output /dev/null -c $DIR/cookies.txt --compressed -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d "{\"method\": \"auth.login\", \"params\": [\"${deluge_password}\"], \"id\": 1}" http://localhost:8112/json
+
+http_code=$(curl -s -o $DIR/add_torrent_error.log -w "%{http_code}" -b $DIR/cookies.txt --compressed -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST -d "{\"method\": \"core.add_torrent_magnet\", \"params\": [\"${magnet_url}\", {\"move_completed_path\": \"${move_completed_path}\" }], \"id\": 1}" http://localhost:8112/json)
+
+if [[ $http_code -eq 200 ]]; then
+	echo "Torrent added successfully!"
+else
+	cat "$DIR/add_torrent_error.log"
+fi
